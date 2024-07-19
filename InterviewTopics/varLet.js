9d89c00ea980a7a91802d05eb881520ebeac752c
Amansoni3 <soni.aman9039@gmail.com>
2024-07-19 20:35:26 +0530

    cinfig

--- InterviewTopics/varLet.js
+++ InterviewTopics/varLet.js
@@ -1,15 +1,15 @@
-// function varScope() {
-//     if (true) {
-//         var x = 10;
-//     }
-//     console.log(x); // 10 (x is accessible here because var has function scope)
-// }
+function varScope() {
+    if (true) {
+        var x = 10;
+    }
+    console.log(x); // 10 (x is accessible here because var has function scope)
+}
 
-// varScope()
+varScope()
 
-// function letScope() {
-//     if (true) {
-//         let y = 10;
-//     }
-//     console.log(y); // ReferenceError: y is not defined (y is not accessible here because let has block scope)
-// }
+function letScope() {
+    if (true) {
+        let y = 10;
+    }
+    console.log(y); // ReferenceError: y is not defined (y is not accessible here because let has block scope)
+}
